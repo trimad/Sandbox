02b7a9f79a2374190e85223973b990ee7e664773
Fractalnaut <fractalnaut@nousresearch.com>
2026-08-02 09:02:52 -0400

    Daily fractal: Octic Multibrot (2026-08-02)

--- Program.cs
+++ Program.cs
@@ -761,6 +761,11 @@ namespace Sandbox
                             .Init(settings.MinReal, settings.MaxReal, settings.MinImaginary, settings.MaxImaginary);
                         hasFractalData = false;
                         break;
+                    case "octic-multibrot": //fractal
+                        fractal = new OcticMultibrot(settings.Width, settings.Height, settings.Bailout, settings.Highest)
+                            .Init(settings.MinReal, settings.MaxReal, settings.MinImaginary, settings.MaxImaginary);
+                        hasFractalData = false;
+                        break;
                     case "newton": //fractal
                         fractal = new NewtonFractal(settings.Width, settings.Height, settings.Highest, 3)
                             .Init(-1.5, 1.5, -1.5, 1.5);
@@ -1028,6 +1033,7 @@ namespace Sandbox
                         Console.WriteLine(String.Format("{0,-24} {1}", "quintic-multibrot", "Render the Quintic Multibrot / degree-5 Mandelbrot-family fractal"));
                         Console.WriteLine(String.Format("{0,-24} {1}", "sextic-multibrot", "Render the Sextic Multibrot / degree-6 Mandelbrot-family fractal"));
                         Console.WriteLine(String.Format("{0,-24} {1}", "septic-multibrot", "Render the Septic Multibrot / degree-7 Mandelbrot-family fractal"));
+                        Console.WriteLine(String.Format("{0,-24} {1}", "octic-multibrot", "Render the Octic Multibrot / degree-8 Mandelbrot-family fractal"));
                         Console.WriteLine(String.Format("{0,-24} {1}", "newton", "Render the Newton fractal (z^3-1)"));
                         Console.WriteLine(String.Format("{0,-24} {1}", "celtic", "Render the Celtic Mandelbrot fractal"));
                         Console.WriteLine(String.Format("{0,-24} {1}", "perpendicular-mandelbrot", "Render the Perpendicular Mandelbrot fractal"));
